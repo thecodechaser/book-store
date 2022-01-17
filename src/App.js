6e1bad76ff0befc6d7069a1d3b8730d8206336d5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-17 20:32:05 +0530

    Add Books component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Books from './components/Books';
 
 function App() {
   return (
     <div className="App">
      <p>Hello</p>
+     
     </div>
   );
 }
---
+++ src/components/Books.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Books = () =>{
+    return(
+        <div>Hello</div>
+    );
+}
+
+export default Books;
