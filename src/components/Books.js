cc3ad9e08dbbc22766d37a08ceb98b66c6365ecb
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-17 21:08:21 +0530

    Add markup for Books

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,7 +3,15 @@ import React from "react";
 const Books = () =>{
     return(
         <main>
-            
+            <div>
+
+            </div>
+            <h2>ADD NEW BOOK</h2>
+            <form >
+                <input type="text" placeholder="Book title"></input>
+                <input type="text" placeholder="Category"></input>
+                <button type="submit">Add Book</button>
+            </form>
         </main>
     );
 }
