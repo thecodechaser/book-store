53e372e39f35bb5abed54d889dd74d1bc3cbd970
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-17 20:53:02 +0530

    Add main component under router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Books from './components/Books';
 import Categories from './components/Categories';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
