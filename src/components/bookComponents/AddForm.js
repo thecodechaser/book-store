30eb64dd4720233af3f51c247766cd37e770400d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 18:59:08 +0530

    Add events for add book

--- src/components/bookComponents/AddForm.js
+++ src/components/bookComponents/AddForm.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../../redux/books/books';
+import { v4 as uuidv4 } from 'uuid';
 
 const dispatch = useDispatch();
+const [title, setTitle] = useState('');
+const [author, setAuthor] = useState('');
 
-const addBook =()=>{
+const addBook =(e)=>{
+   e.preventDefault();
+   if(title === '' || author === '') return;
     const newBook ={
-        title: ,
-        id:
+        id: uuidv4(),
+        title,
+        author
     }
 
     dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
 }
 
 const AddForm = () => (
   <form className="form">
-    <input className="input-title" type="text" placeholder="Book title" />
+    <input required className="input-title" type="text" placeholder="Book title..." value={title}
+    onChange={(e) => setTitle(e.target.value)}/>
+    <input required className="input-author" type="text" placeHolder="Book author..." value={author}
+    onChange={(e) => setAuthor(e.target.value)}/>
     <input className="input-category" type="text" placeholder="Category" />
     <button className="add-book-btn" type="submit" onClick={addBook}>Add Book</button>
   </form>
