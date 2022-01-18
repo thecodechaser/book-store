313a5bf7da69d67eefb1348d432c59b93cf8d53d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 13:10:42 +0530

    Add BookList component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import AddForm from './bookComponents/AddForm';
+import BookList from './bookComponents/BookList';
 
 const Books = () => (
   <main>
-    <div />
+    <BookList />
     <h2 className="add-book-h2">ADD NEW BOOK</h2>
     <AddForm />
   </main>
---
+++ src/components/bookComponents/BookList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BookList = () => (
+  <div>
+    <h2>Book List</h2>
+  </div>
+);
+
+export default BookList;
