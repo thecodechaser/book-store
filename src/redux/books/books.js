db0b2b2fe83f2a05be1260ae6cfc8cbb73418bbf
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 18:25:27 +0530

    Add reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -12,3 +12,16 @@ export const removeBook = payload => ({
     type: REMOVE_BOOK,
     payload
 })
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case ADD_BOOK:
+            return [...state, action.payload];
+            case REMOVE_BOOK:
+                return state.filter(book => book.id !==id);
+                default :
+                return state;
+    }
+}
+
+export default reducer;
