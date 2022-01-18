c5762f2b9498733f8794ef64707711d0a2a38f9a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 18:29:10 +0530

    Add useDispatch

--- src/components/bookComponents/AddForm.js
+++ src/components/bookComponents/AddForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 
+const dispatch = useDispatch();
 const AddForm = () => (
   <form className="form">
     <input className="input-title" type="text" placeholder="Book title" />
