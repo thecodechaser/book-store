c8b54002f0861591530d40260ec36a7568686528
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 13:07:59 +0530

    Add AddForm component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import AddForm from './bookComponents/AddForm';
 
 const Books = () => (
   <main>
     <div />
     <h2 className="add-book-h2">ADD NEW BOOK</h2>
-    <form className="form">
-      <input className="input-title" type="text" placeholder="Book title" />
-      <input className="input-category" type="text" placeholder="Category" />
-      <button className="add-book-btn" type="submit">Add Book</button>
-    </form>
+    <AddForm />
   </main>
 );
 
