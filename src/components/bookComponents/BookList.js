34a8513fd0f0297930383ab45afd8cf8955cbc57
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 22:22:22 +0530

    Add styling for book

--- src/components/bookComponents/BookList.js
+++ src/components/bookComponents/BookList.js
@@ -6,13 +6,12 @@ const BookList = () => {
   const books = useSelector((state) => state.bookReducer);
   return (
     <div>
-      <h2 className="booklist-h2">Book List</h2>
       {books.length ? (
         books.map((book) => (
           <BookItem book={book} key={book.id} />
         ))
       ) : (
-        <h3>There is not books</h3>
+        <h3 className="booklist-h3">There is not books</h3>
       )}
     </div>
   );
