3c016b1104a7597c8cd09e1c3f9916620d01a34d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 18:20:53 +0530

    Add actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,3 +2,13 @@ const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const initialState = [];
+
+export const addBook = payload => ({
+    type: ADD_BOOK,
+    payload
+});
+
+export const removeBook = payload => ({
+    type: REMOVE_BOOK,
+    payload
+})
