1473072003bd499d14c093714eefdcf6f2597ad1
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 22:18:44 +0530

    Add action to remove book

--- src/components/bookComponents/BookItem.js
+++ src/components/bookComponents/BookItem.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 
 const BookItem = (props) => {
   const {
     book: {
-      title,
+      id, title, author,
     },
   } = props;
+ 
   return (
     <div className="book-container">
       <h3 className="book-title">
         {title}
       </h3>
-      <button className="remove-btn" type="button">Remove</button>
+      <h3 className="book-title">
+        {author}
+      </h3>
+      <button className="remove-btn" type="button" onClick={removeBookFromStore}>Remove</button>
     </div>
   );
 };
 
 BookItem.propTypes = {
   book: PropTypes.shape({
+    id: PropTypes.string,
     title: PropTypes.string,
+    author: PropTypes.string,
   }).isRequired,
 };
 
