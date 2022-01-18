c2870e75b751128e8243230f5cf152e4c93530fd
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-18 18:33:32 +0530

    Add addBook fucntion

--- src/components/bookComponents/AddForm.js
+++ src/components/bookComponents/AddForm.js
@@ -3,11 +3,21 @@ import { useDispatch } from 'react-redux';
 import { addBook } from '../../redux/books/books';
 
 const dispatch = useDispatch();
+
+const addBook =()=>{
+    const newBook ={
+        title: ,
+        id:
+    }
+
+    dispatch(addBook(newBook));
+}
+
 const AddForm = () => (
   <form className="form">
     <input className="input-title" type="text" placeholder="Book title" />
     <input className="input-category" type="text" placeholder="Category" />
-    <button className="add-book-btn" type="submit">Add Book</button>
+    <button className="add-book-btn" type="submit" onClick={addBook}>Add Book</button>
   </form>
 );
 
