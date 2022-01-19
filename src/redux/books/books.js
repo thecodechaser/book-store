9d80c48c36afc6e4cceec49ce413cf38320cc6f2
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-20 01:00:19 +0530

    Fix remove book

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -62,7 +62,7 @@ const reducer = (state = initialState, action) => {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload.id);
+      return state.filter((book) => book.item_id !== action.payload);
     case FETCH_BOOK:
       return action.payload;
     default:
