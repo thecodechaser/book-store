b5ce36142ebd0d32531a04a826a1479d3abdc1a8
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-20 00:42:10 +0530

    Fix fetchbook

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -6,6 +6,8 @@ const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
 
+const initialState = [];
+
 export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
@@ -37,15 +39,14 @@ export const fetchBookApi = () => async (dispatch) => {
   }
 };
 
-const initialState = [];
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
-      
+      case FETCH_BOOK:
+        return action.payload;
     default:
       return state;
   }
