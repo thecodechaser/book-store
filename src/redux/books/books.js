89cce430eaf51198855554b884686e4a12c540ee
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-19 21:34:49 +0530

    Add fetch book action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
 
 const initialState = [];
 
@@ -10,6 +11,11 @@ export const addBook = (payload) => ({
 
 export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
+  payload,
+});
+
+const fetchBook = (payload) => ({
+  type: FETCH_BOOK,
   payload,
 });
 
