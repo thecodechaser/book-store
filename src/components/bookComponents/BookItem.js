ae5478719951d08b6911e82304edf4ed621f837f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-01-20 19:29:30 +0530

    Fix alignment6

--- src/components/bookComponents/BookItem.js
+++ src/components/bookComponents/BookItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { removeBookApi } from '../../redux/books/books';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { removeBookApi } from '../../redux/books/books';
 
 const BookItem = (props) => {
   const {
@@ -23,19 +23,17 @@ const BookItem = (props) => {
           {title}
         </h3>
         <button className="remove-btn" type="button" onClick={removeBookFromStore}>Remove</button>
-          <h3 className="book-author">
-        {category}
-      </h3>
+        <h3 className="book-author">
+          {category}
+        </h3>
       </div>
       <div className="progress-bar">
-      <CircularProgressbar
-            value={percentage}
-            styles={buildStyles({ pathColor: '#0EA5E9', marginRight: '10px' })}
-          />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({ pathColor: '#0EA5E9', marginRight: '10px' })}
+        />
       </div>
-      <div>
-
-      </div>
+      <div />
     </div>
   );
 };
